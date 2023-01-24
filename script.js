d9495d7aeba59f1dba7a35f010af9c7c25415a9e
ElenaStr13 <estrateluk@yahoo.com>
2023-01-24 10:12:40 +0200

    function+object

--- script.js
+++ script.js
@@ -3,64 +3,120 @@
  1.2 додати властивість type: "electric" (або вкажіть інше значення)
  1.3 створіть метод signal, при запуску якого буде виводитися alert("fa!fa!")*/
 
-let car = {
-    model: "renault",
-    year: 2015,
-    color: "orange",
-};
+// let car = {
+//     model: "renault",
+//     year: 2015,
+//     color: "orange",
+// };
 
-car.color = "brown";
-console.log(`Car-color: ${car.color}`)
+// car.color = "brown";
+// console.log(`Car-color: ${car.color}`)
 
-car.type = "electric";
-console.log(`Car-type: ${car.type}`)
+// car.type = "electric";
+// console.log(`Car-type: ${car.type}`)
 
-car.signal = function () {
-    console.log("fa!fa!");
-};
+// car.signal = function () {
+//     console.log("fa!fa!");
+// };
 
-car.signal();
+// car.signal();
 
 /*2.Переписати метод total() об'єкта salaries  таким чином, щоб він мав можливість 
 рахувати зп незалежно від кількості працівників.*/
 
-let salaries = {
-    frontend: 12000,
-    backend: 10000,
-    designer: 8000,
-    dayPay() {
-        console.log('We must pay salary on Tuesday!');
-    },
-    total: function () {
-        let sum = 0;
-        for (let salary of Object.values(salaries)) {
-            if (typeof (salary) == 'number') {
-                sum += salary;
-            } else {
-                continue
-            }
-        }
-        console.log(sum);
-        return sum;
-    }
-}
+// let salaries = {
+//     frontend: 12000,
+//     backend: 10000,
+//     designer: 8000,
+//     dayPay() {
+//         console.log('We must pay salary on Tuesday!');
+//     },
+//     total: function () {
+//         let sum = 0;
+//         for (let salary of Object.values(salaries)) {
+//             if (typeof (salary) == 'number') {
+//                 sum += salary;
+//             } else {
+//                 continue
+//             }
+//         }
+//         console.log(sum);
+//         return sum;
+//     }
+// }
 
-salaries.total();
+// salaries.total();
 
 
 /*3. Створити функцію конструктор та побудувати за допомогою неї 2 об'єкти*/
 
-function Notebook(brand, system, cost) {
-    this.brand = brand;
-    this.system = system;
-    this.cost = cost;
-};
+// function Notebook(brand, system, cost) {
+//     this.brand = brand;
+//     this.system = system;
+//     this.cost = cost;
+// };
 
-let dell = new Notebook('Dell', 'windows', '800');
-let apple = new Notebook('Apple', 'MAC OS', '1700');
+// let dell = new Notebook('Dell', 'windows', '800');
+// let apple = new Notebook('Apple', 'MAC OS', '1700');
 
-console.log(dell);
-console.log(apple);
+// console.log(dell);
+// console.log(apple);
 
 /*4.*/
+// let num1 = 5//prompt("Enner first number");
+// let num2 = 2//prompt("Enner second number");
+
+// let Operation = function(a,b) {
+// if (a % 2 == 0 && b % 2 == 0) {
+//     result = a * b;
+//     console.log(result);
+// } else if (a % 2 !== 0 && b % 2 !== 0) {
+// result = a + b;
+// console.log(result);
+// } else if (a % 2 !== 0 && b % 2 == 0) {
+//     console.log(a);
+// } else if (a % 2 == 0 && b % 2 !== 0) {
+//     console.log(b);
+// }
+// };
+// Operation(num1,num2);
+
+// Objects
+let newWorker;
+
+function add() {
+    let worker = prompt("Enter name new worker");
+    for (const key in tasks) {
+        if (worker == key) {
+            worker = prompt('This worker is busy, please enter another name');
+            newWorker = worker;
+        } else {
+            newWorker = worker;
+        };
+    };
+    console.log(newWorker);
+    return newWorker;
+};
+
+console.log(newWorker);
+const tasks = {
+    Anna: {
+        status: 'new',
+        text: 'Create checkout page',
+    },
+    Ihor: {
+        status: 'done',
+        text: 'Add styles to header',
+    },
+    [newWorker]: {
+        status: 0,
+        text: "none",
+    },
+};
+
+add(newWorker, tasks);
+let task = { ...tasks };
+console.log(task);
+// newWorker.text = prompt("Enter what work need to do");
+// newWorker.status = prompt("Enter status this work new/done");
 
