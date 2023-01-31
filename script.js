74cc82d387b26d2ee3691bc62a203389f2b08907
ElenaStr13 <estrateluk@yahoo.com>
2023-01-31 12:21:30 +0200

    massiv

--- script.js
+++ script.js
@@ -1,140 +1,89 @@
-/*Створити об'єкт car, який буде містити властивості model, year, color
-1.1 замініти значення кольору на інший;
- 1.2 додати властивість type: "electric" (або вкажіть інше значення)
- 1.3 створіть метод signal, при запуску якого буде виводитися alert("fa!fa!")*/
+//1.Створити масив користувачів, вивести 2 елемент, замінити Ніколу на Алекс
+// Видалити два останніх елемента, вивести довжину масиву, вивід всього у консоль
 
-// let car = {
-//     model: "renault",
-//     year: 2015,
-//     color: "orange",
-// };
+const user = ["Mike", "Nikola", "Tom"];
+console.log (user[2]);
 
-// car.color = "brown";
-// console.log(`Car-color: ${car.color}`)
+let n = user.indexOf("Nikola");
+user[n] = "Alex";
+console.log (user);
 
-// car.type = "electric";
-// console.log(`Car-type: ${car.type}`)
+user.splice(1,2);
+console.log (user);
+console.log(user.length);
 
-// car.signal = function () {
-//     console.log("fa!fa!");
-// };
+//2.Написати функцію, яка приймає два числові масиви сумує їх
+// та виводить результат у консоль а>b, a<b чи a==b
 
-// car.signal();
+ let a = [5, 3, -4, 25, 32, -16, 6];
+ let b = [21, -30, 9, 5, 12, -19, 5, 25];
 
-/*2.Переписати метод total() об'єкта salaries  таким чином, щоб він мав можливість 
-рахувати зп незалежно від кількості працівників.*/
+function result(a,b)
+{let sumA = a.reduce(function(sum,elem) {
+    return sum + elem;
+}, 0);
+console.log(sumA);
+let sumB = b.reduce(function(sum,elem) {
+    return sum + elem;
+}, 0);
+console.log(sumB);
+if (sumA > sumB) {
+    console.log("a > b");
+} else if (sumA < sumB) {
+    console.log("a < b");
+} else {
+    console.log("a==b");
+}
+};
+result(a,b);
 
-// let salaries = {
-//     frontend: 12000,
-//     backend: 10000,
-//     designer: 8000,
-//     dayPay() {
-//         console.log('We must pay salary on Tuesday!');
-//     },
-//     total: function () {
-//         let sum = 0;
-//         for (let salary of Object.values(salaries)) {
-//             if (typeof (salary) == 'number') {
-//                 sum += salary;
-//             } else {
-//                 continue
-//             }
-//         }
-//         console.log(sum);
-//         return sum;
-//     }
-// }
+//3.Зробити масив із фрази так, щоб кожне окреме слово було елементом масиву
 
-// salaries.total();
+let phrase = 'I am learning JavaScript right now';
 
+let words = phrase.split(" ");
+console.log(words);
 
-/*3. Створити функцію конструктор та побудувати за допомогою неї 2 об'єкти*/
+//4. Дано два масиви. За допомогою методу foreach додайте у масив b всі елементи
+// масиву а, але без повторів. Потрібно отримати масив [5,3,8,2,1]
 
-// function Notebook(brand, system, cost) {
-//     this.brand = brand;
-//     this.system = system;
-//     this.cost = cost;
-// };
+let a = [5, 3, 8, 5, 3, 2, 1, 2];
+let b = [];
 
-// let dell = new Notebook('Dell', 'windows', '800');
-// let apple = new Notebook('Apple', 'MAC OS', '1700');
-
-// console.log(dell);
-// console.log(apple);
-
-/*4.*/
-// let num1 = 5//prompt("Enner first number");
-// let num2 = 2//prompt("Enner second number");
-
-// let Operation = function(a,b) {
-// if (a % 2 == 0 && b % 2 == 0) {
-//     result = a * b;
-//     console.log(result);
-// } else if (a % 2 !== 0 && b % 2 !== 0) {
-// result = a + b;
-// console.log(result);
-// } else if (a % 2 !== 0 && b % 2 == 0) {
-//     console.log(a);
-// } else if (a % 2 == 0 && b % 2 !== 0) {
-//     console.log(b);
-// }
-// };
-// Operation(num1,num2);
-
-// Objects
-let newWorker;
-let worker = prompt("Enter name new worker");
-
-function add() {
-    for (let key in tasks) {
-        if (worker == key) {
-            worker = prompt('This worker is busy, please enter another name');
-            newWorker = worker;
-        } else {
-            newWorker = worker;
-        };
-
+function result(arr){
+    for (let i of arr) {
+    if(!b.includes(i)) {
+        b.push(i);
     };
-    return newWorker;
+}return b;
+};
+b = result(a);
+console.log(b);
+
+/*5. Написати функцію, яка знайде та виведе у консоль всі номери id 
+об'єктів, в яких age понад 18 та менше, як 21, не включаючи 18 та 21.
+Таким чином: 3, 6*/
+
+let users = [{ id: 1, age: 17 },
+{ id: 2, age: 18 },
+{ id: 3, age: 19 },
+{ id: 4, age: 21 },
+{ id: 5, age: 17 },
+{ id: 6, age: 20 },
+{ id: 7, age: 25 },];
+
+function selectionAge(item) {
+  if (item.age > 18 && item.age < 21) {
+    console.log(item.id);
+    return true;
+  }
+    return false;
 };
 
-const tasks = {
-    Anna: {
-        status: 'new',
-        text: 'Create checkout page',
-    },
-    Ihor: {
-        status: 'done',
-        text: 'Add styles to header',
-    },
-    [newWorker]: {
-        status: 0,
-        text: "none",
-    },
-};
+ users.filter(selectionAge);
 
-add();
 
-// function taske() {
-//     for (let value in tasks) {
-//         let text = prompt("Enter what work need to do");
-//         let status = prompt("Enter status this work new/done");
-//         tasks[newWorker].text = text;
-//         if (status == "done") {
-//             status = "new";
-//             tasks[newWorker].status = status;
-//         } else {
-//             tasks[newWorker].status = status;
-//         };
-//     };
-//     return tasks;
-// };
-// taske();
-console.log(newWorker);
-let task = { ...tasks };
-console.log(task);
-// task.newWorker.text = prompt("Enter what work need to do");
-// task.newWorker.status = prompt("Enter status this work new/done");
-// console.log(task);
+  
+
 
 
