6bce61a61a177b0a24706244c45988ed10ec1233
ElenaStr13 <estrateluk@yahoo.com>
2023-02-04 19:56:50 +0200

    recurse

--- script.js
+++ script.js
@@ -1,89 +1,89 @@
-//1.Створити масив користувачів, вивести 2 елемент, замінити Ніколу на Алекс
-// Видалити два останніх елемента, вивести довжину масиву, вивід всього у консоль
+/*1.Задано масив let arr = [1,2,3,4,5,6,7,8,9,10], використовуючи
+рекурсію написати функцію знаходження суми всіх елементів масиву*/
 
-const user = ["Mike", "Nikola", "Tom"];
-console.log (user[2]);
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-let n = user.indexOf("Nikola");
-user[n] = "Alex";
-console.log (user);
+console.log(sumM(arr));
 
-user.splice(1,2);
-console.log (user);
-console.log(user.length);
-
-//2.Написати функцію, яка приймає два числові масиви сумує їх
-// та виводить результат у консоль а>b, a<b чи a==b
-
- let a = [5, 3, -4, 25, 32, -16, 6];
- let b = [21, -30, 9, 5, 12, -19, 5, 25];
-
-function result(a,b)
-{let sumA = a.reduce(function(sum,elem) {
-    return sum + elem;
-}, 0);
-console.log(sumA);
-let sumB = b.reduce(function(sum,elem) {
-    return sum + elem;
-}, 0);
-console.log(sumB);
-if (sumA > sumB) {
-    console.log("a > b");
-} else if (sumA < sumB) {
-    console.log("a < b");
-} else {
-    console.log("a==b");
+function sumM(arr) {
+    return sum(arr, arr.length - 1);
 }
-};
-result(a,b);
-
-//3.Зробити масив із фрази так, щоб кожне окреме слово було елементом масиву
-
-let phrase = 'I am learning JavaScript right now';
-
-let words = phrase.split(" ");
-console.log(words);
-
-//4. Дано два масиви. За допомогою методу foreach додайте у масив b всі елементи
-// масиву а, але без повторів. Потрібно отримати масив [5,3,8,2,1]
-
-let a = [5, 3, 8, 5, 3, 2, 1, 2];
-let b = [];
-
-function result(arr){
-    for (let i of arr) {
-    if(!b.includes(i)) {
-        b.push(i);
+function sum(arr, i) {
+    if (i == 0) {
+        return arr[0];
+    } else {
+        return arr[i] + sum(arr, i - 1);
     };
-}return b;
-};
-b = result(a);
-console.log(b);
-
-/*5. Написати функцію, яка знайде та виведе у консоль всі номери id 
-об'єктів, в яких age понад 18 та менше, як 21, не включаючи 18 та 21.
-Таким чином: 3, 6*/
-
-let users = [{ id: 1, age: 17 },
-{ id: 2, age: 18 },
-{ id: 3, age: 19 },
-{ id: 4, age: 21 },
-{ id: 5, age: 17 },
-{ id: 6, age: 20 },
-{ id: 7, age: 25 },];
-
-function selectionAge(item) {
-  if (item.age > 18 && item.age < 21) {
-    console.log(item.id);
-    return true;
-  }
-    return false;
 };
 
- users.filter(selectionAge);
+//2. Написати функцію deepCount(arr), яка повертає кількість усіх
+// елементів у масиві, включаючи вкладені масиви
+
+const a = [],
+    b = [1, 2, 3],
+    c = ["x", "y", ["z"]],
+    d = [1, 2, [3, 4, [5]]],
+    e = [[[[]]]];
+
+let counte = 0;
+function recursion(m) {
+    for (const elem of m) {
+        if (Array.isArray(elem)) {
+            counte++;
+            recursion(elem);
+        } else {
+            counte++
+        }
+    }
+    return count
+};
+console.log(recursion(d));//7
+console.log(recursion(a));
+console.log(recursion(b));
+console.log(recursion(c));
+console.log(recursion(e));
 
 
-  
+//3. Переписати функцію sumSallary так, щоб змінна sum
+//була ізольована від глобальної області бачення
 
+let employees = {
+    development: {
+        backend: [{ name: 'Mike', salary: 2000 }, { name: 'Nikola', salary: 2500 }],
+        frontend: [{ name: 'Artem', salary: 3000 }, { name: 'Alex', salary: 2700 }],
+    },
+    sales: {
+        marketing: {
+            internet_marketers: [{ name: 'Nina', salary: 1000 }, { name: 'Olena', salary: 1300 }],
+            promotion: [{ name: 'Oleg', salary: 1400 }, { name: 'Masha', salary: 1700 }],
+        },
+        sellers: [{ name: 'Max', salary: 900 }, { name: 'Donald', salary: 700 }, { name: 'Joe', salary: 1100 }]
+    },
+    designer: [{ name: 'Kate', salary: 1800 }]
+}
 
+function sumSallary(obj) {
+    let sum = 0;
+    for (let key in obj) {
+        if (Array.isArray(obj[key])) {
+            return obj[key].forEach(employees => sum += employees.salary);
 
+        } else {
+            return sumSallary(obj[key]);
+        }
+
+    }
+     return sum;
+}
+
+sumSallary(employees);
+
+/*4. Вбудована у JavaScript функція Math.max() повертає найбільше значення
+серед переданих аргументів функції. Знайти найбільше значення серед трьох масивів*/
+
+let arr1 = [2, 15, 7, 3],
+    arr2 = [9, 3, 17, 12, 4, 8],
+    arr3 = [6, 11, 16, 15, 11];
+
+let count = [...arr1, ...arr2, ...arr3];
+console.log(Math.max( ...count));
