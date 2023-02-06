8e5e8a14a5adb0c304b32c18f8fbcc6ed0ec9def
ElenaStr13 <estrateluk@yahoo.com>
2023-02-06 10:33:06 +0200

    oop

--- script.js
+++ script.js
@@ -1,89 +1,62 @@
-/*1.Задано масив let arr = [1,2,3,4,5,6,7,8,9,10], використовуючи
-рекурсію написати функцію знаходження суми всіх елементів масиву*/
+/*1. В класі  User зробити захищену властивість  name  і публічні
+властивості login  та age. Завдяки класу створити 2 об'єкти user1, user2*/
+/*2. Створити у класі  User  метод  getName(isAdmin), який повертає ім'я
+користувача або його нік, якщо ім'я не вказано, або відмовляє у доступі, 
+якщо аргумент isAdmin == false*/
+/*3. Створити у класі  User  метод  ChangeName, змінює ім'я
+користувача. Змінна повинна бути підтвердженна паролем 123, якщо ні,
+тоді виводиться Permission denied*/
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-console.log(sumM(arr));
-
-function sumM(arr) {
-    return sum(arr, arr.length - 1);
-}
-function sum(arr, i) {
-    if (i == 0) {
-        return arr[0];
-    } else {
-        return arr[i] + sum(arr, i - 1);
-    };
-};
-
-//2. Написати функцію deepCount(arr), яка повертає кількість усіх
-// елементів у масиві, включаючи вкладені масиви
-
-const a = [],
-    b = [1, 2, 3],
-    c = ["x", "y", ["z"]],
-    d = [1, 2, [3, 4, [5]]],
-    e = [[[[]]]];
-
-let counte = 0;
-function recursion(m) {
-    for (const elem of m) {
-        if (Array.isArray(elem)) {
-            counte++;
-            recursion(elem);
-        } else {
-            counte++
-        }
+class User {
+    #name;
+    constructor(name, login, age) {
+        this.#name = name;
+        this.login = login;
+        this.age = age;
     }
-    return count
-};
-console.log(recursion(d));//7
-console.log(recursion(a));
-console.log(recursion(b));
-console.log(recursion(c));
-console.log(recursion(e));
-
-
-//3. Переписати функцію sumSallary так, щоб змінна sum
-//була ізольована від глобальної області бачення
-
-let employees = {
-    development: {
-        backend: [{ name: 'Mike', salary: 2000 }, { name: 'Nikola', salary: 2500 }],
-        frontend: [{ name: 'Artem', salary: 3000 }, { name: 'Alex', salary: 2700 }],
-    },
-    sales: {
-        marketing: {
-            internet_marketers: [{ name: 'Nina', salary: 1000 }, { name: 'Olena', salary: 1300 }],
-            promotion: [{ name: 'Oleg', salary: 1400 }, { name: 'Masha', salary: 1700 }],
-        },
-        sellers: [{ name: 'Max', salary: 900 }, { name: 'Donald', salary: 700 }, { name: 'Joe', salary: 1100 }]
-    },
-    designer: [{ name: 'Kate', salary: 1800 }]
-}
-
-function sumSallary(obj) {
-    let sum = 0;
-    for (let key in obj) {
-        if (Array.isArray(obj[key])) {
-            return obj[key].forEach(employees => sum += employees.salary);
-
-        } else {
-            return sumSallary(obj[key]);
-        }
-
+    getName(isAdmin) {
+        return isAdmin ? this.#name : "Permission denied"
     }
-     return sum;
+    ChangeName(newName, loginNew) {
+        if (loginNew == '123') {
+            this.login = loginNew;
+            this.#name = newName;
+            console.log(`Name Changed from Mike to ${newName}`);
+            return this.#name
+        } else {
+            console.log("Premission denied");
+        }
+        return this.#name
+    }
 }
 
-sumSallary(employees);
+let user1 = new User('Mike', 'Mk_18', 18);
+let user2 = new User('', 'NRG', 22);
 
-/*4. Вбудована у JavaScript функція Math.max() повертає найбільше значення
-серед переданих аргументів функції. Знайти найбільше значення серед трьох масивів*/
+console.log(user1.login);
+console.log(user1.age);
+console.log(user2.login);
+console.log(user2.age);
 
-let arr1 = [2, 15, 7, 3],
-    arr2 = [9, 3, 17, 12, 4, 8],
-    arr3 = [6, 11, 16, 15, 11];
+console.log(user1.getName(true));//Mike
+console.log(user2.getName(true));//
+console.log(user2.getName(false));//Mike
+console.log(user1.ChangeName("Bill", '123'));
 
-let count = [...arr1, ...arr2, ...arr3];
-console.log(Math.max( ...count));
+/*4. Зробити клас Admin, розширюючи його класом  User. Має приватну властивість #isAdmin = true
+Зробити в цьому класі  getUserName(user), який буде приймати користувача та виводити його ім'я*/
+
+class Admin extends User {
+    #isAdmin;
+    constructor(name, login, age, isAdmin = true) {
+        super(name, login, age);
+        this.#isAdmin = isAdmin;
+}
+getUserName(userOne) {
+       return userOne.getName(true);
+    }
+}
+
+let user3 = new Admin ('Olena', '123', 37);
+
+console.log(user3.getUserName(user3)); //Olena
